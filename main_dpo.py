4be68598493780f5ca2de86a3f193800f63253d1
zfy <91236358+fyzhang1@users.noreply.github.com>
2024-08-06 09:50:15 +0800

    Add files via upload

--- main_dpo.py
+++ main_dpo.py
@@ -9,11 +9,35 @@ import torch
 from datasets import Dataset, load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from trl import DataCollatorForCompletionOnlyLM, DPOTrainer
-from peft import get_peft_model, get_peft_model_state_dict, set_peft_model_state_dict, prepare_model_for_kbit_training, AutoPeftModelForCausalLM
+from peft import get_peft_model, get_peft_model_state_dict, set_peft_model_state_dict, prepare_model_for_kbit_training, \
+    AutoPeftModelForCausalLM
 
+from utils import load
 from utils import *
 from federated_learning import *
 from config import get_config, save_config, get_model_config, get_training_args
+
+# edit begin
+'''
+--model_name_or_path pretrained_model/ehartford/Wizard-Vicuna-7B-Uncensored/
+--dataset_name Anthropic/hh-rlhf
+--dataset_sample 100
+--fed_alg fedavg
+--num_clients 5
+--sample_clients 1
+--learning_rate 5e-4
+--max_steps 5
+--num_rounds 1
+--batch_size 8
+--gradient_accumulation_steps 1
+--seq_length 512
+--use_peft
+--load_in_8bit
+--output_dir output
+--template vicuna_v1.1
+--local_data_dir /root/autodl-tmp/0722_code_OpenFedLLM/dataset/
+'''
+# edit end
 
 # ===== Define the arguments =====
 script_args, fed_args, peft_config = get_config()
@@ -22,11 +46,13 @@ save_config(script_args, fed_args)
 print(script_args, fed_args)
 
 # ===== Load the dataset =====
-dataset = get_dataset(script_args.dataset_name, script_args.local_data_dir)
-dataset = process_dpo_dataset(script_args.dataset_name, dataset, script_args.template, script_args.dataset_sample)
+# dataset = get_dataset(script_args.dataset_name, script_args.local_data_dir)
+# dataset = process_dpo_dataset(script_args.dataset_name, dataset, script_args.template, script_args.dataset_sample)
 
 # ===== Split the dataset into clients =====
-local_datasets = split_dataset(fed_args, script_args, dataset)
+# local_datasets = split_dataset(fed_args, script_args, dataset)
+attack_file_path = "/root/autodl-tmp/0722_code_OpenFedLLM/dataset/Attack_dataset/untargeted/untargeted_all_dataset.csv"
+local_datasets = load.load_attack_data(attack_file_path)
 sample_num_list = [len(local_datasets[i]) for i in range(fed_args.num_clients)]
 
 # ===== Get model config =====
@@ -55,8 +81,8 @@ else:
 
 if script_args.load_in_8bit or script_args.load_in_4bit:
     model = prepare_model_for_kbit_training(
-                model, use_gradient_checkpointing=training_args.gradient_checkpointing
-            )
+        model, use_gradient_checkpointing=training_args.gradient_checkpointing
+    )
 
 model = get_peft_model(model, peft_config)
 model.print_trainable_parameters()
@@ -75,7 +101,7 @@ global_auxiliary, auxiliary_model_list, auxiliary_delta_dict = get_auxiliary_dic
 # ===== Define the tokenizer =====
 tokenizer = AutoTokenizer.from_pretrained(script_args.model_name_or_path, use_fast=False, padding_side="right")
 if tokenizer.pad_token is None:
-    tokenizer.pad_token = tokenizer.unk_token   # following vicuna
+    tokenizer.pad_token = tokenizer.unk_token  # following vicuna
 
 # ===== Start federated training =====
 training_loss = [[] for i in range(fed_args.num_clients)]
@@ -85,18 +111,20 @@ for round in tqdm(range(fed_args.num_rounds)):
 
     clients_this_round = get_clients_this_round(fed_args, round)
 
-    print(f">> ==================== Round {round+1} : {clients_this_round} ====================")
-    
+    print(f">> ==================== Round {round + 1} : {clients_this_round} ====================")
+
     for client in range(fed_args.num_clients):
 
         if client not in clients_this_round:
-            training_loss[client].append(-1)        # -1 is an indicator of not training
+            training_loss[client].append(-1)  # -1 is an indicator of not training
             continue
 
-        set_peft_model_state_dict(model, global_dict)   # sync the global model to the local model
+        set_peft_model_state_dict(model, global_dict)  # sync the global model to the local model
 
-        sub_dataset = get_dataset_this_round(local_datasets[client], round, fed_args, script_args)      # get the required sub-dataset for this round
-        new_lr = cosine_learning_rate(round, fed_args.num_rounds, script_args.learning_rate, 1e-5)      # manually schedule the learning rate
+        sub_dataset = get_dataset_this_round(local_datasets[client], round, fed_args,
+                                             script_args)  # get the required sub-dataset for this round
+        new_lr = cosine_learning_rate(round, fed_args.num_rounds, script_args.learning_rate,
+                                      1e-5)  # manually schedule the learning rate
         training_args = get_training_args(script_args, new_lr)
 
         # ===== Train local model on the client side =====
@@ -104,8 +132,8 @@ for round in tqdm(range(fed_args.num_rounds)):
             script_args, fed_args, model, model_ref, \
             tokenizer, training_args, sub_dataset, global_dict, \
             auxiliary_model_list[client], global_auxiliary
-            )
-        
+        )
+
         results = trainer.train()
         training_loss[client].append(results.training_loss)
 
@@ -113,14 +141,26 @@ for round in tqdm(range(fed_args.num_rounds)):
         if fed_args.fed_alg == 'scaffold':
             auxiliary_model_list[client], auxiliary_delta_dict[client] = trainer.get_auxiliary_param()
 
-        local_dict_list[client] = copy.deepcopy(get_peft_model_state_dict(model))   # copy is needed!
+        local_dict_list[client] = copy.deepcopy(get_peft_model_state_dict(model))  # copy is needed!
 
     # ===== Aggregate the local models =====
-    global_dict, global_auxiliary = global_aggregate(fed_args, global_dict, local_dict_list, sample_num_list, clients_this_round, round, proxy_dict=proxy_dict, opt_proxy_dict=opt_proxy_dict, auxiliary_info=(global_auxiliary, auxiliary_delta_dict))
-    set_peft_model_state_dict(model, global_dict)   # update global model
+    global_dict, global_auxiliary = global_aggregate(fed_args, global_dict, local_dict_list, sample_num_list,
+                                                     clients_this_round, round, proxy_dict=proxy_dict,
+                                                     opt_proxy_dict=opt_proxy_dict,
+                                                     auxiliary_info=(global_auxiliary, auxiliary_delta_dict))
+    set_peft_model_state_dict(model, global_dict)  # update global model
 
     # ===== Save the model =====
-    if (round+1) % fed_args.save_model_freq == 0:
-        trainer.save_model(os.path.join(script_args.output_dir, f"checkpoint-{round+1}"))
-    
+    # edit begin
+    fed_args.save_model_freq = 50
+    # edit end
+    # ===== 每十轮保存一次 =====
+    if (round+1) % 10 == 0:
+        np.save(os.path.join(script_args.output_dir, f"global_model_round_{round+1}.npy"), global_dict)
+        for client_id in range(fed_args.num_clients):
+            np.save(os.path.join(script_args.output_dir, f"local_model_client_{client_id}_round_{round+1}.npy"), local_dict_list[client_id])
+
+    if (round + 1) % fed_args.save_model_freq == 0:
+        trainer.save_model(os.path.join(script_args.output_dir, f"checkpoint-{round + 1}"))
+
     np.save(os.path.join(script_args.output_dir, "training_loss.npy"), np.array(training_loss))
